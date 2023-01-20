28001900c3e7cb872a9d53efe1f50ad58009dffa
猪指盐 <zhuzhiyan@zhuzhiyandeMBP-2.local>
2023-01-20 01:36:04 -0800

    ini

---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(239,233,222,10)',
+    fontWeight: 'bold',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
